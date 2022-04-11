0f9a2321dc5152b37ca997faff3b7d1f6e59d917
Bilaal Salman <mbilaalme@gmail.com>
2022-04-11 21:58:49 +0100

    Added and initialized hive.

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,17 @@ import 'package:countdown_app/features/add_countdown/presentation/bloc/add_count
 import 'package:countdown_app/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() {
   BlocOverrides.runZoned(
-    () {
+    () async {
+
+      await Hive.initFlutter();
+      
       runApp(MyApp());
     },
-    blocObserver: CounterObserver()
+    blocObserver: CountdownObserver()
   );
 }
 
