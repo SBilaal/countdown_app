a7368cc3f911fe2f1acf44d2febf078ba95a952e
Bilaal Salman <mbilaalme@gmail.com>
2022-06-12 17:13:48 +0100

    feat: Created repository class for countdown.

--- lib/features/add_countdown/data/repositories/countdown_repository.dart
+++ lib/features/add_countdown/data/repositories/countdown_repository.dart
@@ -1,3 +1,24 @@
+import 'package:countdown_app/features/add_countdown/data/datasources/countdown_store.dart';
+import 'package:countdown_app/features/add_countdown/data/models/countdown.dart';
+
 class CountdownRepository {
-  
+  final CountdownStore countdownStore;
+
+  CountdownRepository({required this.countdownStore});
+
+  Future<List<Countdown>> getAllCountdowns() async {
+    return countdownStore.getAllCountdowns();
+  }
+
+  Future<void> addCountdown(Countdown countdown) async {
+    await countdownStore.addCountdown(countdown);
+  }
+
+  Future<void> updateCountdown(Countdown countdown) async {
+    await countdownStore.updateCountdown(countdown);
+  }
+
+  Future<void> deleteCountdown(Countdown countdown) async {
+    await countdownStore.deleteCountdown(countdown);
+  }
 }
