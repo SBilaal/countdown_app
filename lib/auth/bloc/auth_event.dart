046ca7e7ff49fb9865406003e8b5c8b1c03323b8
Bilaal Salman <mbilaalme@gmail.com>
2022-10-06 12:57:56 +0100

    feat: Added authcheckrequested union to check if user is authenticated at app start up.

--- lib/auth/bloc/auth_event.dart
+++ lib/auth/bloc/auth_event.dart
@@ -1,9 +1,9 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 
-part 'auth_event.freezed.dart';
+part of 'auth_bloc.dart';
 
 @freezed
 class AuthEvent with _$AuthEvent {
-  const factory AuthEvent.loginButtonPressed() = LoginButtonPressed;
-  const factory AuthEvent.logoutButtonPressed() = LogoutButtonPressed;
+  const factory AuthEvent.loginButtonPressed() = _LoginButtonPressed;
+  const factory AuthEvent.logoutButtonPressed() = _LogoutButtonPressed;
+  const factory AuthEvent.authCheckRequested() = _AuthCheckRequested;
 }
