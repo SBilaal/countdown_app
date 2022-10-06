c18c559e366daf5b2937f51c4801f1496012e3fa
Bilaal Salman <mbilaalme@gmail.com>
2022-10-06 13:00:16 +0100

    feat: Created authentication service.

---
+++ lib/core/services/firebase_auth_service.dart
@@ -0,0 +1,36 @@
+import 'package:countdown_app/auth/user.dart' as i1;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future signIn() async {
+    try {
+      final googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) return;
+      final googleAuth = await googleUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final result =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      return result.user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future<i1.User?> getUser() async {
+    final user = _firebaseAuth.currentUser;
+    return user != null ? i1.User(id: user.uid) : null;
+  }
+
+  Future<void> signOut() async {
+    await _googleSignIn.signOut();
+    await FirebaseAuth.instance.signOut();
+  }
+}
