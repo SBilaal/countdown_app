9a14b6b5a717a878d44c8174ef558076b0866283
Bilaal Salman <mbilaalme@gmail.com>
2023-12-14 14:14:14 +0100

    refactor: Pages renamed.

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,8 +1,9 @@
+// ignore_for_file: prefer_const_constructors
 
-import 'package:countdown_app/features/add_countdown/presentation/pages/edit_screen.dart';
-import 'package:countdown_app/features/add_countdown/presentation/pages/event_screen.dart';
+import 'package:countdown_app/features/add_countdown/presentation/pages/countdown_form_page.dart';
+import 'package:countdown_app/features/add_countdown/presentation/pages/countdown_overview_page.dart';
 
 final routes = {
-  '/': (context) => EventsScreen(),
-  '/edit_screen': (context) => EditScreen(),
+  '/': (context) => CountdownOverviewPage(),
+  CountdownFormPage.name: (context) => CountdownFormPage(),
 };
