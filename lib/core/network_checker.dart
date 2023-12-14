09d9deff13a03e772c0ef1c87568b3a638f099f1
Bilaal Salman <mbilaalme@gmail.com>
2023-12-14 14:54:39 +0100

    feat: Made this an injectable.

--- lib/core/network_checker.dart
+++ lib/core/network_checker.dart
@@ -1,5 +1,7 @@
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:injectable/injectable.dart';
 
+@injectable
 class NetworkInfo {
   InternetConnectionChecker connectionChecker = InternetConnectionChecker();
 
